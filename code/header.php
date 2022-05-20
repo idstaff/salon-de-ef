d6896c1ceb1001fc3377ea3d1c8891f071c6b52a
TomoyukiShiraishi <touroku@s-ideal.net>
2022-05-20 15:10:40 +0900

    ヘッダーおよびフッター部分のアセット化

---
+++ code/header.php
@@ -0,0 +1,65 @@
+<header class="header">
+    <div class="header-inner">
+        <h1 class="header-logo">
+            <a href="index.html">
+                <img width="85" height="21" src="./img/top/logo-ef.svg" alt="salondef" decoding="async" loading="lazy"/>
+            </a>
+        </h1>
+        <!--スマホ時のみ現れるCVボタン-->
+        <div class="header-hamburger-cv-btn u-sp">
+            <a href="./contact.html">
+                お問い合わせ
+            </a>
+        </div>
+        <!--ボタン用-->
+        <div class="openbtn1"><span></span><span></span><span></span></div>
+        <nav class="header-nav">
+            <!--縦スクロール用-->
+            <div class="nav-list">
+            <ul>
+                <li class="border"><a href="/">トップ</a></li>
+                <!--階層有-->
+                <li class="has-child border li-eyelash"><a href="#">まつ毛</a>
+                    <ul>
+                        <li><a href="./eyelash-extensions.html">まつ毛エクステ</a></li>
+                        <li><a href="./eyelash-perm.html">まつ毛パーマ</a></li>
+                    </ul>
+                </li>
+                <!--階層有-->
+                <li class="has-child border"><a href="#">脱毛</a>
+                    <ul>
+                    <li><a href="#">IPL脱毛</a></li>
+                    <li><a href="#">SHR脱毛</a></li>
+                    <li><a href="#">ブラジリアン<br class="u-pc">ワックス</a></li>
+                    </ul>
+                </li>
+                <!--階層有-->
+                <li class="has-child border"><a href="#">エステティック</a>
+                    <ul>
+                        <li><a href="#">マジョール</a></li>
+                        <li><a href="#">スーパー<br class="u-pc">リフトアップ</a></li>
+                        <li><a href="#">スロータス</a></li>
+                        <li><a href="#">リボーニング</a></li>
+                        <li><a href="#">カップル美顔</a></li>
+                        <li><a href="#">BBL</a></li>
+                    </ul>
+                </li>
+                <li class="border"><a href="./about.html">サロン案内</a></li>
+                <li><a href="./blog-top.html">ブログ</a></li>
+                <li class="u-pc header-pc-contact"><a href="./contact.html">お問い合わせ</a></li>
+            </ul>
+            <div class="u-sp sp-hamberger-cv-zone">
+                <div class="sp-hamberger-cv-zone-tel">
+                    <p>0985-85-3904</p>
+                    <small>10:00〜18:00 月曜日定休日</small>
+                </div>
+                <div class="sp-hamberger-cv-zone-line">
+                    <a href="#">
+                        <img src="./img/top/pic-line.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+        </nav>
+    </div>
+</header>
